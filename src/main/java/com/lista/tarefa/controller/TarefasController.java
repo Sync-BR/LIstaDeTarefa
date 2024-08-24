38775ce9f2399d42cf35238ef2c56f73e66398e6
Sync <eduardofreitasfr@gmail.com>
2024-08-24 12:59:47 -0300

    Adicionado novas funcionalidades Atualizar lista. Deleta lista

--- src/main/java/com/lista/tarefa/controller/TarefasController.java
+++ src/main/java/com/lista/tarefa/controller/TarefasController.java
@@ -4,71 +4,77 @@ import com.lista.tarefa.model.TarefaModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.swing.text.html.parser.Entity;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class TarefasController {
+    private static List<TarefaModel> listTarefas = new ArrayList<>();
     public TarefasController() {
         listTarefas = new ArrayList<>();
     }
 
-    private static List<TarefaModel> listTarefas = new ArrayList<>();
-
     @PostMapping("/cadastrar")
-    public String cadastrarTarefas(@RequestBody TarefaModel addTarefa) {
-        listTarefas.add(new TarefaModel(addTarefa));
-        System.out.println(listTarefas);
-        return "Tarefa adicionada com sucesso!";
-
+    public ResponseEntity<String> cadastrarTarefas(@RequestBody TarefaModel addTarefa) {
+        if(addTarefa.getId() == 0){
+            addTarefa.setId(listTarefas.size() + 1);
+        }
+        boolean status = listTarefas.add(new TarefaModel(addTarefa));
+        if (status) {
+            return new ResponseEntity<>("Tarefa adicionada com sucesso!", HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>("Erro ao adicionar uma tarefa!", HttpStatus.CONFLICT);
+        }
     }
 
-    private static TarefaModel findById(int id) {
+    private static int findById(int id) {
+        int size = 0;
         for (TarefaModel tarefa : listTarefas) {
+            size++;
             if (tarefa.getId() == id) {
-                return tarefa;
+                return size - 1;
             }
         }
-        return null;
+        return 0;
+    }
+    @DeleteMapping("/deletar{id}")
+    public ResponseEntity<String> deletarTarefa(@PathVariable int id) {
+        int index = findById(id);
+        int sizeAtual = listTarefas.size();
+        listTarefas.remove(index);
+        int novoSize = listTarefas.size() ;
+        if(sizeAtual == novoSize) {
+            return new ResponseEntity<>("Não foi possivel remover a tarefa!", HttpStatus.CONFLICT);
+        } else if(novoSize < sizeAtual){
+            return new ResponseEntity<>("Tarefa removida com sucesso!", HttpStatus.ACCEPTED);
+        } else {
+            return new ResponseEntity<>("Erro desconhecido", HttpStatus.NOT_ACCEPTABLE);
+        }
     }
 
-    public void atualizarTarefa(@RequestBody int id, TarefaModel updateTarefa) {
-        TarefaModel tarefa = findById(id);
-        System.out.println(tarefa);
-
+    @PutMapping("/update")
+    public static void atualizarTarefa(@RequestBody int id, TarefaModel updateTarefa) {
+        int index = findById(id);
+        listTarefas.set(index, updateTarefa);
     }
 
     @GetMapping("/tarefas")
     public ResponseEntity<List> retonarTarefa() {
-        return new ResponseEntity<>(listTarefas, HttpStatus.OK);
+        if(listTarefas.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else{
+            return new ResponseEntity<>(listTarefas, HttpStatus.OK);
+        }
+
     }
 
     public static void main(String[] args) {
-        TarefaModel tarefa1 = new TarefaModel();
-        tarefa1.setId(1);
-        tarefa1.setTitulo("Tarefa 1");
-        tarefa1.setDescricao("Tarefa 1");
-        tarefa1.setStatus(false);
-        TarefaModel tarefa2 = new TarefaModel();
-        tarefa2.setId(2);
-        tarefa2.setTitulo("Tarefa 2");
-        tarefa2.setDescricao("Tarefa 1");
-        tarefa2.setStatus(false);
-        TarefaModel tarefa3 = new TarefaModel();
-        tarefa3.setId(3);
-        tarefa3.setTitulo("Tarefa 2");
-        tarefa3.setDescricao("Tarefa 1");
-        tarefa3.setStatus(false);
-        listTarefas.add(new TarefaModel(tarefa1));
-        listTarefas.add(new TarefaModel(tarefa2));
-        listTarefas.add(new TarefaModel(tarefa3));
-        TarefaModel tarefa = findById(3);
-        System.out.println(tarefa);
-    }
+        if(listTarefas.isEmpty()){
+            System.out.println("Estou vazio");
+        }
 
+    }
 }
 
 
