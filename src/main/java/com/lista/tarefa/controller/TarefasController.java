256e06f52faff3ef940d8d481c2f027a3973a840
Sync <eduardofreitasfr@gmail.com>
2024-08-28 14:39:52 -0300

    Corrigido bug da posição do array

--- src/main/java/com/lista/tarefa/controller/TarefasController.java
+++ src/main/java/com/lista/tarefa/controller/TarefasController.java
@@ -4,6 +4,7 @@ import com.lista.tarefa.model.TarefaModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,22 +12,21 @@ import java.util.List;
 @RequestMapping("/api")
 public class TarefasController {
     private static List<TarefaModel> listTarefas = new ArrayList<>();
-    private static List<TarefaModel> tarefasConcluidas = new ArrayList<>();
+
     public TarefasController() {
         listTarefas = new ArrayList<>();
-        tarefasConcluidas = new ArrayList<>();
     }
 
     @PostMapping("/cadastrar")
-    public ResponseEntity<String> cadastrarTarefas(@RequestBody TarefaModel addTarefa) {
-        if(addTarefa.getId() == 0){
+    public ResponseEntity<HttpStatus> cadastrarTarefas(@RequestBody TarefaModel addTarefa) {
+        if (addTarefa.getId() == 0) {
             addTarefa.setId(listTarefas.size() + 1);
         }
         boolean status = listTarefas.add(new TarefaModel(addTarefa));
         if (status) {
-            return new ResponseEntity<>("Tarefa adicionada com sucesso!", HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
         } else {
-            return new ResponseEntity<>("Erro ao adicionar uma tarefa!", HttpStatus.CONFLICT);
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
     }
 
@@ -34,67 +34,41 @@ public class TarefasController {
         int size = 0;
         for (TarefaModel tarefa : listTarefas) {
             size++;
+            System.out.println(tarefa.getId());
             if (tarefa.getId() == id) {
                 return size - 1;
             }
         }
-        return 0;
+        return size;
     }
 
-    private static void concluir( int id){
-        int size = findById(id);
-
-        for(TarefaModel tarefa : listTarefas){
-
-            if(tarefa.getId() == id){
-                tarefa.setStatus(true);
-                listTarefas.set(size, tarefa);
-                break;
-            }
-        }
-
-    }
     @PostMapping("/concluirid{id}")
-    public ResponseEntity<String>concluirTarefa(@PathVariable int id){
-        concluir(id);
-        return new ResponseEntity<>("Tarefa concluida com sucesso!", HttpStatus.OK);
+    public ResponseEntity<HttpStatus> concluirTarefa(@PathVariable int id) {
+        listTarefas.remove(findById(id));
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/deletar{id}")
-    public ResponseEntity<String> deletarTarefa(@PathVariable int id) {
+    public ResponseEntity<HttpStatus> deletarTarefa(@PathVariable int id) {
         int index = findById(id);
         int sizeAtual = listTarefas.size();
         listTarefas.remove(index);
-        int novoSize = listTarefas.size() ;
-        if(sizeAtual == novoSize) {
-            return new ResponseEntity<>("Não foi possivel remover a tarefa!", HttpStatus.CONFLICT);
-        } else if(novoSize < sizeAtual){
-            return new ResponseEntity<>("Tarefa removida com sucesso!", HttpStatus.ACCEPTED);
+        int novoSize = listTarefas.size();
+        if (sizeAtual == novoSize) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        } else if (novoSize < sizeAtual) {
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
         } else {
-            return new ResponseEntity<>("Erro desconhecido", HttpStatus.NOT_ACCEPTABLE);
+            return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
-    }
-
-    @PutMapping("/update")
-    public static void atualizarTarefa(@RequestBody TarefaModel updateTarefa) {
-        System.out.println("Teste");
-        int index = findById(updateTarefa.getId());
-        listTarefas.set(index, updateTarefa);
     }
 
     @GetMapping("/tarefas")
     public ResponseEntity<List> retonarTarefa() {
-        if(listTarefas.isEmpty()) {
+        if (listTarefas.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } else{
+        } else {
             return new ResponseEntity<>(listTarefas, HttpStatus.OK);
-        }
-
-    }
-
-    public static void main(String[] args) {
-        if(listTarefas.isEmpty()){
-            System.out.println("Estou vazio");
         }
 
     }
